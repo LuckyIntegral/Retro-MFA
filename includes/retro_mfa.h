44fa64ff99e6fa9d4e786dbf0721e0bb813a1c58
Michael Heider <miheider@c3r8p3.42vienna.com>
2024-03-16 13:08:46 +0100

    just started

--- includes/retro_mfa.h
+++ includes/retro_mfa.h
@@ -10,5 +10,21 @@
 # include <string.h>
 # include <unistd.h>
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+typedef struct s_mfa
+{
+	void	*mlx_connection;
+	void	*mlx_window;
+	char	*name;
+	t_data	image;
+}	t_mfa
 
 #endif // RETRO_MFA_H
--- srcs/main.c
+++ srcs/main.c
@@ -3,16 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vfrants <vfrants@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: miheider <miheider@42>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 23:16:29 by vfrants           #+#    #+#             */
-/*   Updated: 2024/03/15 23:36:38 by vfrants          ###   ########.fr       */
+/*   Updated: 2024/03/16 12:49:52 by miheider         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/retro_mfa.h"
 
+void	init(t_mfa *mfa)
+{
+	initialize mlx
+}
+
 int main(void) {
+	t_mfa	mfa;
+
+	FILE *file;
+
+	file = fopen(../MFA/green.mfa, O_RDONLY);
+	if(!file)
+	{
+		printf("Error: Opening file!\n");
+		exit (1);
+	}
+
+
+/*	- read file into buffer.
+	- parse for meta data. (but how?)
+	- decode
+	- display with mlx
+	- get full points
+*/
+
+	fclose(file);
+
+
+	init(&mfa);							//initialize mlx
 	write(1, "Hello, World!\n", 14);
 	return 0;
 }
